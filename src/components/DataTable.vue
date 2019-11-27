cd4a5995443e637a4080b277fc01ca649209edb9
maitungmn <goingdownfr@gmail.com>
2019-11-27 16:23:21 +0700

    [Datatable] Finish

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -23,13 +23,15 @@
       <v-icon small class="mr-2" @click="editItem(item)">mdi-table-edit</v-icon>
       <v-icon small @click="deleteItem(item)">mdi-delete</v-icon>
     </template>
-    <template v-slot:no-data>
+    <!-- <template v-slot:no-data>
       <v-btn color="primary" @click="initialize">Reset</v-btn>
-    </template>
+    </template>-->
   </v-data-table>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
   components: {
     CreateAndEditDialog: () => import("@/components/CreateAndEditDialog")
@@ -49,8 +51,8 @@ export default {
       { text: "Protein (g)", value: "protein" },
       { text: "Actions", value: "action", sortable: false }
     ],
-    desserts: [],
     editedIndex: -1,
+    desserts: [],
     editedItem: {
       name: "",
       calories: 0,
@@ -69,6 +71,9 @@ export default {
   }),
 
   computed: {
+    ...mapGetters({
+      dessertsFromStore: "dataTable/desserts"
+    }),
     formTitle() {
       return this.editedIndex === -1 ? "New Item" : "Edit Item";
     }
@@ -86,89 +91,16 @@ export default {
           ? (this.isShowSaveBtn = true)
           : (this.isShowSaveBtn = false);
       }
+    },
+    dessertsFromStore: {
+      deep: true,
+      immediate: true,
+      handler(newVal) {
+        this.desserts = newVal;
+      }
     }
   },
-
-  created() {
-    this.initialize();
-  },
-
   methods: {
-    initialize() {
-      this.desserts = [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7
-        }
-      ];
-    },
-
     editItem(item) {
       this.editedIndex = this.desserts.indexOf(item);
       this.editedItem = Object.assign({}, item);
@@ -195,6 +127,7 @@ export default {
       } else {
         this.desserts.push(this.editedItem);
       }
+      this.$store.commit("dataTable/setDesserts", this.desserts);
       this.close();
     }
   }
